608f7395f73cb21f3af608a9b66a674939d50e72
Sarthak <sarthakmjh@gmail.com>
2024-08-05 14:12:23 +0530

    hosting

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCF_AoL5H_KD22Df4yO0cDRaQPK-T9slnc",
+    authDomain: "aninfo-e3110.firebaseapp.com",
+    projectId: "aninfo-e3110",
+    storageBucket: "aninfo-e3110.appspot.com",
+    messagingSenderId: "867940010680",
+    appId: "1:867940010680:web:7d8e1d218a8586f22b024d",
+    measurementId: "G-8HX57TBPEN"
+  };
+
+ 
